d7e15b368cb17f0643160a27f891ca9c7b2eb383
gregory-saucerotte <gregory.saucerotte@gmail.com>
2018-04-09 12:36:28 +0100

    modif page d'accueil

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/include/initialisation.php';
 
 include __DIR__ . '/layout/top.php';
 ?>
-<h1>Accueil</h1>
+<h1>Page d'accueil</h1>
 <?php
 include __DIR__ . '/layout/bottom.php';
 ?>
